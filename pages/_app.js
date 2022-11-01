85739dc0eb1cf1a7626342195c41d1b847d2878c
SvetozarMiskovic <svetomiskovic98@gmail.com>
2022-11-01 03:56:23 +0100

    more changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,48 +3,17 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import GlobalContext from "../context/GlobalContext";
-import { useEffect, useState } from "react";
-import { ThreeCircles } from "react-loader-spinner";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  const updateLoading = () => {
-    const t = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(t);
-  };
-
-  useEffect(() => {
-    updateLoading();
-  }, []);
-
   return (
     <>
-      {!loading ? (
-        <GlobalContext>
-          <ChakraProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </ChakraProvider>
-        </GlobalContext>
-      ) : (
-        <ThreeCircles
-          height="100"
-          width="100"
-          color="#4fa94d"
-          wrapperStyle={{}}
-          wrapperClass="loading-screen"
-          visible={true}
-          ariaLabel="three-circles-rotating"
-          outerCircleColor="#007FFF "
-          innerCircleColor="#4CBB17"
-          middleCircleColor="#f89a20"
-        />
-      )}
+      <GlobalContext>
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </GlobalContext>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,50 @@
-import Categories from "../components/Categories";
-import FirstProductLine from "../components/FirstProductLine";
 import Home from "../components/Home";
-import SecondProductLine from "../components/SecondProductLine";
-import SponsoredProducts from "../components/SponsoredProducts";
-import { db } from "../util/db.server";
+import { useEffect, useState } from "react";
+import { ThreeCircles } from "react-loader-spinner";
 
 export default function LandingPage({ res }) {
-  console.log(res);
+  const [loading, setLoading] = useState(true);
+
+  const updateLoading = () => {
+    const t = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(t);
+  };
+
+  useEffect(() => {
+    updateLoading();
+  }, []);
+
   return (
     <div className="homepage">
-      <Home />
+      {loading ? (
+        <Home />
+      ) : (
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#4fa94d"
+          wrapperStyle={{}}
+          wrapperClass="loading-screen"
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor="#007FFF "
+          innerCircleColor="#4CBB17"
+          middleCircleColor="#f89a20"
+        />
+      )}
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const res = await db.kategorije.findMany();
+// export async function getServerSideProps() {
+//   const res = await db.kategorije.findMany();
 
-  return {
-    props: {
-      res,
-    },
-  };
-}
+//   return {
+//     props: {
+//       res,
+//     },
+//   };
+// }
