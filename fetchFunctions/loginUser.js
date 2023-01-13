5093b143b8d8a5e1ff45ae2be70642b2cae51211
SvetozarMiskovic <svetomiskovic98@gmail.com>
2023-01-14 00:23:01 +0100

    to see is it working

--- fetchFunctions/loginUser.js
+++ fetchFunctions/loginUser.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const loginUser = async data => {
-  const url = "http://localhost:3000/api/login";
+  const url = "http://pc-expert.vercel.app:3000/api/login";
 
   const result = await axios.post(url, data);
 
