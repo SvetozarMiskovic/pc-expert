70bec8b54570985f60325cff4145f32b2cf6f2b7
SvetozarMiskovic <svetomiskovic98@gmail.com>
2023-01-14 00:31:28 +0100

    some more changes

--- fetchFunctions/checkLogin.js
+++ fetchFunctions/checkLogin.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const checkLogin = async () => {
   const url = "https://pc-expert.vercel.app:3000/api/hello";
 
-  const result = await axios.get(url);
+  const result = await axios(url);
 
   return result;
 };
