647850e4368f50dd0d974fa647510dc1672964ca
SvetozarMiskovic <svetomiskovic98@gmail.com>
2023-01-14 00:55:48 +0100

    more changes

--- fetchFunctions/loginUser.js
+++ fetchFunctions/loginUser.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const loginUser = async data => {
-  const url = "https://pc-expert.vercel.app:3000/api/login";
+  const url = "https://pc-expert.vercel.app/api/login";
 
   const result = await axios.post(url, data);
 
