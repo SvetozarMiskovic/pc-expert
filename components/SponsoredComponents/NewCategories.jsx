ba701c201a2687ed8d602350a1c4c122ba15b002
SvetozarMiskovic <svetomiskovic98@gmail.com>
2023-01-18 22:44:34 +0100

    added popup on categories

--- components/SponsoredComponents/NewCategories.jsx
+++ components/SponsoredComponents/NewCategories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaMemory,
   FaMobileAlt,
@@ -19,10 +19,20 @@ import {
   Divider,
   Text,
   Icon,
+  Fade,
+  Button,
 } from "@chakra-ui/react";
 function NewCategories() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggle = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
   return (
     <div className="new-categories-wrapper">
+      {/* <Button onClick={onToggle}>Click Me</Button> */}
+
       <Tabs
         variant="line"
         backgroundColor={"#fff"}
@@ -31,94 +41,192 @@ function NewCategories() {
         boxShadow={"0 0 6px 0 rgb(1 1 1 / 30%)"}
       >
         <TabList overflowX={"auto"} overflowY={"hidden"}>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaLaptop} />
             <Text fontSize={"md"}>Laptopi</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaDesktop} />
             <Text fontSize={"md"}>Monitori</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaWindows} />
             <Text fontSize={"md"}>Računari</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaTv} />
             <Text fontSize={"md"}>Televizori</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaMobileAlt} />
             <Text fontSize={"md"}>Telefoni</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaMemory} />
             <Text fontSize={"md"}>Računarske komponente</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaKeyboard} />
             <Text fontSize={"md"}>Računarska periferija</Text>
           </Tab>
-          <Tab display={"flex"} alignItems="center" gap={"0.4rem"}>
+          <Tab
+            onClick={() => {
+              if (isOpen) {
+                return;
+              } else {
+                onToggle();
+              }
+            }}
+            onDoubleClick={onToggle}
+            display={"flex"}
+            alignItems="center"
+            gap={"0.4rem"}
+          >
             <Icon as={FaBitcoin} />
             <Text fontSize={"md"}>Mining </Text>
           </Tab>
         </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Text fontSize={"2xl"}>Proizvođač</Text>
-            <ul>
-              <div className="value">
-                <h5>Acer</h5>
-              </div>
-              <div className="value">
-                <h5>Asus</h5>
-              </div>
-              <div className="value">
-                <h5>Dell</h5>
-              </div>
-              <div className="value">
-                <h5>Apple</h5>
-              </div>
-            </ul>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={"2xl"}>Proizvođač</Text>
+        <Fade in={isOpen}>
+          <TabPanels display={isOpen ? "block" : "none"}>
+            <TabPanel>
+              <Text fontSize={"2xl"}>Proizvođač</Text>
+              <ul>
+                <div className="value">
+                  <h5>Acer</h5>
+                </div>
+                <div className="value">
+                  <h5>Asus</h5>
+                </div>
+                <div className="value">
+                  <h5>Dell</h5>
+                </div>
+                <div className="value">
+                  <h5>Apple</h5>
+                </div>
+              </ul>
+            </TabPanel>
+            <TabPanel>
+              <Text fontSize={"2xl"}>Proizvođač</Text>
 
-            <ul>
-              <div className="value">
-                <h5>Acer</h5>
-              </div>
-              <div className="value">
-                <h5>Asus</h5>
-              </div>
-              <div className="value">
-                <h5>AOC</h5>
-              </div>
-              <div className="value">
-                <h5>Dell</h5>
-              </div>
-            </ul>
-          </TabPanel>
-          <TabPanel>
-            <p>Računari</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Televizori</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Telefoni</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Računarske komponente</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Računarska periferija</p>
-          </TabPanel>
-          <TabPanel>
-            <p>Mining</p>
-          </TabPanel>
-        </TabPanels>
+              <ul>
+                <div className="value">
+                  <h5>Acer</h5>
+                </div>
+                <div className="value">
+                  <h5>Asus</h5>
+                </div>
+                <div className="value">
+                  <h5>AOC</h5>
+                </div>
+                <div className="value">
+                  <h5>Dell</h5>
+                </div>
+              </ul>
+            </TabPanel>
+            <TabPanel>
+              <p>Računari</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Televizori</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Telefoni</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Računarske komponente</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Računarska periferija</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Mining</p>
+            </TabPanel>
+          </TabPanels>
+        </Fade>
       </Tabs>
     </div>
   );
