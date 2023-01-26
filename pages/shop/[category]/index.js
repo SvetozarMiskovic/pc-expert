c1802ee88f07c657b86733f5e11e784d8d264c2c
SvetozarMiskovic <svetomiskovic98@gmail.com>
2023-01-26 20:27:28 +0100

    sad ne radi nista tek

--- pages/shop/[category]/index.js
+++ pages/shop/[category]/index.js
@@ -23,14 +23,12 @@ function Category({ cat, data }) {
 export default Category;
 
 export async function getServerSideProps(context) {
-  const rez = await whatToFetch(context.query.category, db);
-
-  if (rez) {
-    return {
-      props: {
-        cat: context.query.category,
-        data: !!rez && rez,
-      },
-    };
-  }
+  // const rez = await whatToFetch(context.query.category, db);
+  // !!rez && rez
+  return {
+    props: {
+      cat: context.query.category,
+      data: "sveto",
+    },
+  };
 }
